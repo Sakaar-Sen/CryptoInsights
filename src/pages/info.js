72199451e4c3213ff572b57b5b6f4cdb579e6f54
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-11 21:20:59 +0530

    s

--- src/pages/info.js
+++ src/pages/info.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Info() {
   return (
     <div>
       <Navbar />

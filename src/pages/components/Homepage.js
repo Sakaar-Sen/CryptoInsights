65975e6b4aefc5a3b1416e242b03afb48e19501a
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-13 20:27:16 +0530

    e

--- src/pages/components/Homepage.js
+++ src/pages/components/Homepage.js
@@ -7,22 +7,15 @@ const HomePage = () => {
   return (
     <div className={styles.container}>
       <div className="flex center items-center flex-col-reverse gap-6 md:flex-row md:gap-32">
-        <div className="text-white flex-column align-middle text-center center">
-          <p className="text-white text-3xl md:text-6xl">CRYPTO INSIGHTS</p>
-          <p className="text-1xl md:text-[1.4rem] mt-6 mb-6">
-            Your go to place for crypto information
-          </p>
+        <div className="text-white flex-column align-middle text-center center md:mr-[14rem] lg:mr-[36rem]">
+          <p className="text-white text-4xl md:text-6xl">CRYPTO INSIGHTS</p>
+          <p className="text-[1.2rem] md:text-[1.4rem] mt-6 mb-6">Decode the Crypto Maze</p>
           <div className=" flex center items-center justify-center gap-12">
             <button className="bg-blue-500 hover:bg-blue-700 text-white text-1xl md:text-1xl font-bold py-2 px-4 rounded-full">
               Get Started
             </button>
-
-            {/* <button className="bg-blue-500 hover:bg-blue-700 text-white text-[1rem] md:text-1xl font-bold py-2 px-4 rounded-full">
-              Docs
-            </button> */}
           </div>
         </div>
-        <img src="logo.png" className="min-w-[10rem] md:min-w-[25rem]" />
       </div>
     </div>
   );

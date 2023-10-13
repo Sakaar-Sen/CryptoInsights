e6f493ca6e6e3c7f2fece064f8f3dcf98d4846c2
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-13 21:06:31 +0530

    e

--- src/pages/components/NewsElement.js
+++ src/pages/components/NewsElement.js
@@ -8,7 +8,7 @@ function NewsElement(props) {
 
     return (
         <a href={props.url} target="_blank" className="w-[48rem] hover:scale-105 transition duration-100 relative">
-            <div className="text-white flex items-center justify-start bg-gray-900 border-white border-solid border-2 rounded gap-8">
+            <div className="text-white flex items-center justify-start bg-gray-600 border-white border-solid border-2 rounded gap-8">
                 <div>
                     <Image alt={props.coin} src={"http://sakaarsen.lag.tf/api/image?coin=" + props.coin} width={100} height={100} className="px-6 py-6" />
                 </div>

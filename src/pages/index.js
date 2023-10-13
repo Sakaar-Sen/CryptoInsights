46c51d562e808234f826561d955f99f984d07a91
DKNIGHT41 <118896892+DKNIGHT41@users.noreply.github.com>
2023-10-13 11:17:11 +0530

    homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/Homepage";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HomePage />
     </div>
-   
-  )
+  );
 }

dfa4c3492f71a44d123a5c9fcf6293bb5b5da762
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-13 19:21:00 +0530

    Update chat.js

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -9,28 +9,33 @@ const inter = Inter({ subsets: ["latin"] });
 export default function chatbot() {
 
   const [userMessage, setUserMessage] = useState("")
-  const [botMessage, setBotMessage] = useState("..")
+  const [botMessage, setBotMessage] = useState("")
 
   const sendMessage = async () => {
     setBotMessage("...")
-    const url = "/api/ask";
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({ question: userMessage }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await response.json();
-    setBotMessage(data.answer)
+    try {
+      const url = "/api/ask";
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({ question: userMessage }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+      setBotMessage(data.answer)
+    }
+    catch (error) {
+      setBotMessage("Unable to connect to the server. Please try again!")
+    }
   }
 
-  return (
-    <div>
-      <Navbar />
-          <input type="text" className="text-black text-2xl" onChange={(event) => setUserMessage(event.target.value)} value={userMessage} placeholder="Say something" />
-          <button className="border-solid border-2 border-white text-2xl text-white" onClick={sendMessage}>Send</button>
-          <p className="text-white text-2xl">{botMessage}</p>
-    </div>
-  );
-}
+    return (
+      <div>
+        <Navbar />
+        <input type="text" className="text-black text-2xl" onChange={(event) => setUserMessage(event.target.value)} value={userMessage} placeholder="Say something" />
+        <button className="border-solid border-2 border-white text-2xl text-white" onClick={sendMessage}>Send</button>
+        <p className="text-white text-2xl">{botMessage}</p>
+      </div>
+    );
+  }

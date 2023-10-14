efb812a15b11af3cafcdd00d6ea2c5fd2888be0d
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-14 15:55:36 +0530

    Update NewsElement.js

--- src/pages/components/NewsElement.js
+++ src/pages/components/NewsElement.js
@@ -8,11 +8,12 @@ function NewsElement(props) {
 
     return (
         <a href={props.url} target="_blank" className="md:w-[48rem] hover:scale-105 transition duration-100 relative">
-            <div className="text-white flex items-center justify-start bg-slate-800 border-white border-solid border-2 rounded gap-8">
+            <div className="text-white flex items-center justify-start bg-slate-800 border-gray-500 border-solid border-2 rounded-lg gap-8">
                 <div>
-                    <Image alt={props.coin} src={"http://sakaarsen.lag.tf/api/image?coin=" + props.coin} width={100} height={100} className=" px-2 md:px-6 py-2 md:py-6" />
+                    <Image alt={props.coin} src={"http://sakaarsen.lag.tf/api/image?coin=" + props.coin} width={100} height={100} 
+                    className=" px-2 md:px-6 py-2 md:py-6 opacity-90" />
                 </div>
-                <div className="pr-2 md:pr-4 text-[0.7rem] md:text-[1rem]">
+                <div className="pr-2 md:pr-4 text-[0.7rem] md:text-[1rem] text-gray-200">
                     {props.text}
                     <p className="text-[0.6rem] absolute right-2 md:right-6 bottom-2 md:bottom-3 opacity-70">🕑 {props.time} </p>
                 </div>

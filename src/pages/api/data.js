4af9c7821ae62aac00ed8329fde4393e1045d5d1
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-14 13:46:38 +0530

    Update data.js

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async (req, res) => {
   try {
-    const response = await axios.get('http://127.0.0.1:8000/api/analytics');
+    const response = await axios.get('http://sakaarsen.lag.tf/api/analytics');
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error);

202ae06d35bb06cae2dd500b5ad3c84749d1900a
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2023-10-14 16:17:40 +0530

    Update news.js

--- src/pages/news.js
+++ src/pages/news.js
@@ -8,12 +8,11 @@ import axios from 'axios'
 import styles from './../styles/news.module.css'
 import Chatbot from './components/ChatBot'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function News() {
-
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 15;
 
   useEffect(() => {
     const url = "/api/news";
@@ -24,26 +23,63 @@ export default function News() {
       setNews(data);
       setLoading(false);
     };
-    getNews()
-  }, [])
+    getNews();
+  }, []);
 
+  const totalPages = Math.ceil(news.length / itemsPerPage);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div className={styles.container}>
       <Navbar />
       <Chatbot />
-      <h1 className='text-white text-6xl text-center m-12'>News</h1>
-      <div className='flex align-middle justify-center'>
-        {loading ? <h1 className='text-2xl text-white'>Loading...</h1> :
+      
 
-          <div className="grid grid-cols-1 mt-30 gap-4">
-            {news.map((news) => (
-              <NewsElement text={news.title} coin={news.coin} url={news.url} time={news.time} />
-            ))}
-          </div>
-        }
+      <div className="flex justify-center items-center">
+        <h1 className="text-5xl text-gray-100 font-bold mt-12 mb-4 tracking-wide">News</h1>
       </div>
-    </div>
+      <div className="flex justify-center items-center">
+        <p className="text-gray-300 text-xl mb-8">The latest news from the crypto world</p>
+      </div>
 
-  )
+      
+
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-1 mt-30 gap-4">
+          {news
+            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+            .map((newsItem, index) => (
+              <NewsElement
+                key={index}
+                text={newsItem.title}
+                coin={newsItem.coin}
+                url={newsItem.url}
+                time={newsItem.time}
+              />
+            ))}
+        </div>
+
+
+      </div>
+      {news.length === 0 ? (
+        <div>
+        </div>)
+        :
+        <div className="flex justify-center items-center mt-6 pb-20">
+          <div className='flex gap-24 justify-center items-center'>
+            <button className="text-gray-100 text-[1.3rem] border-gray-300 border-solid border-2 px-2 py-1 rounded-lg bg-slate-900 hover:bg-slate-800 transition-all duration-250" onClick={() => handlePageChange(currentPage - 1)}>Previous </button>
+            <span className='text-white text-[1.3rem]'> {currentPage} of {totalPages} </span>
+            <button className="text-gray-100 text-[1.3rem] border-gray-300 border-solid border-2 px-4 py-1 rounded-lg bg-slate-900 hover:bg-slate-800 transition-all duration-250" onClick={() => handlePageChange(currentPage + 1)}> Next</button>
+          </div>
+        </div>}
+
+
+    </div>
+  );
 }

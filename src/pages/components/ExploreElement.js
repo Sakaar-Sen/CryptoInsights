4176ed0a8f6043e6c13288dee64c4a241df03698
DKNIGHT41 <118896892+DKNIGHT41@users.noreply.github.com>
2023-10-15 18:44:50 +0530

    explore

--- src/pages/components/ExploreElement.js
+++ src/pages/components/ExploreElement.js
@@ -15,22 +15,26 @@ const ExploreElement = () => {
   });
 
   return (
-    <div className="mt-12 grid grid-cols-1  md:grid-cols-3 gap-4 place-items-center mb-12 ">
+    <div className="mt-12 grid grid-cols-1  md:grid-cols-3 gap-4 place-items-center  ">
       {data.map((list, i) => {
         return (
           <div key={i}>
-            <div className="h-52 w-64 rounded-lg  backdrop-blur border-white border-2">
+            <div className="h-52 w-64   backdrop-blur font-bold border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]  mb-12">
               <div className="p-4 text-white">
                 <p>{list.index}</p>
                 <p>{list.price}</p>
                 <div className="mt-2">
                   <div className="flex">
-                    <p className="mr-16 text-slate-500">24h High </p>
-                    <p className=" text-slate-500">$17512.24</p>
+                    <p className="mr-16 text-slate-500">Change 1d </p>
+                    <p className=" text-slate-500">{list.change_1h}</p>
                   </div>
                   <div className="flex">
                     <p className="mr-16 text-slate-500">24h Low </p>
-                    <p className=" text-slate-500">$17512.24</p>
+                    <p className=" text-slate-500">{list.change_1h} </p>
+                  </div>
+                  <div className="flex">
+                    <p className="mr-16 text-slate-500">Volume </p>
+                    <p className=" text-slate-500">{list.volume_1d} </p>
                   </div>
                 </div>
               </div>
--- src/pages/explore.js
+++ src/pages/explore.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
-// import styles from "./../styles/explore.module.css";
+import styles from "./../styles/Explore.module.css";
 import ExploreElement from "./components/ExploreElement";
 
 export default function Explore() {

43aaffe31a9c34f29989fadc0cb9aa30df17be3f
DKNIGHT41 <118896892+DKNIGHT41@users.noreply.github.com>
2023-10-17 23:30:06 +0530

    align

--- src/pages/components/ExploreElement.js
+++ src/pages/components/ExploreElement.js
@@ -23,25 +23,17 @@ const ExploreElement = () => {
               <div className="p-4 text-white z-0">
                 <p>{list.index}</p>
                 <p>{list.price}</p>
-                <div className="mt-2">
-                  <div className="flex">
-                    <p className="mr-16 text-slate-400">Change 1d </p>
-                    <p className=" text-slate-400">{list["Change 1h"]} %</p>
+                <div className=" mt-2 flex">
+                  <div className="mr-16 flex-column">
+                    <p className=" text-slate-400">Change 1d </p>
+                    <p className=" text-slate-400">Change 1h</p>
+                    <p className=" text-slate-400">Volume</p>
+                    <p className=" text-slate-400">Market Cap</p>
                   </div>
-                  <div className="flex">
-                    <p className="mr-16 text-slate-400 z-0">Change 1d </p>
-                    <p className=" text-slate-400">{list["Change 1d"]} %</p>
-                  </div>
-                  <div className="flex">
-                    <p className="mr-16 text-slate-400">Change 1h </p>
+                  <div className="flex-column">
+                    <p className=" text-slate-400 z-0">{list["Change 1d"]}</p>
                     <p className=" text-slate-400">{list["Change 1h"]} </p>
-                  </div>
-                  <div className="flex">
-                    <p className="mr-16 text-slate-400">Volume </p>
-                    <p className=" text-slate-400">{list["Volume 1d"]} </p>
-                  </div>
-                  <div className="flex">
-                    <p className="mr-16 text-slate-400">Market Cap </p>
+                    <p className=" text-slate-400">{list["Volume"]} </p>
                     <p className=" text-slate-400">{list["Market Cap"]} </p>
                   </div>
                 </div>
